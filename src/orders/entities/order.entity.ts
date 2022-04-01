36627c8775f7961078e858b6e2c09cc17c687e1a
yujia <rm4ru8@gmail.com>
2022-04-02 00:32:09 +0800

    crud finish

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, Entity, OneToMany } from "typeorm";
 import { OrdersDetail } from "./ordersdetail.entity";
 
@@ -6,55 +7,63 @@ export class Order {
 
   static REPOSITORY = 'ORDER_REPOSITORY';
 
-  @OneToMany(
-      () => OrdersDetail, 
-      (ordersDetail) => ordersDetail.master,
-      {cascade: true}   // 關聯details用
-    )
-  details: OrdersDetail[];
-
+  @ApiProperty({description:'流程代碼:為系統碼+單號',example:'1O1110218001'})
   @Column({ primary: true, name: "flowkey", length: 50 })
   flowkey: string;
-
+  
+  @ApiProperty({description:'系統碼:一律填寫 1',example:'1'})
   @Column({ name: "sysno", length: 10 })
   sysno: string;
-
+  
+  @ApiProperty({description:'訂單編號',example:'O1110218001'})
   @Column({ name: "code", length: 40 })
   code: string;
 
+  @ApiProperty({description:'申請人',example:'林先生'})
   @Column({ name: "applicant", length: 20 })
   applicant: string;
 
+  @ApiProperty({description:'部門:一侓填寫 B11(商品組)',example:'B11'})
   @Column({ name: "createdept", length: 20 })
   createdept: string;
 
+  @ApiProperty({description:'訂單日期',example:'2022-04-01'})
   @Column({ name: "orderdate", length: 10 })
   orderdate: string;
 
+  @ApiProperty({description:'付款方式(P01:ATM轉帳, P02:超商繳款, P03:信用卡, P04:其他)',example:'P01'})
   @Column({ name: "payment", length: 20 })
   payment: string;
 
+  @ApiProperty({description:'客戶連絡人',example:'林先生'})
   @Column({ name: "customer", length: 50 })
   customer: string;
 
+  @ApiProperty({description:'客戶連絡電話',example:'0987654321'})
   @Column({ name: "telno", length: 20 })
   telno: string;
 
+  @ApiProperty({description:'收件人',example:'陳小姐'})
   @Column({ name: "recipient", length: 50 })
   recipient: string;
 
+  @ApiProperty({description:'收件連絡電話',example:'0987654322'})
   @Column({ name: "contactno", length: 20 })
   contactno: string;
 
+  @ApiProperty({description:'收件地址',example:'臺北市信義區松高路19號7樓'})
   @Column({ name: "address", length: 100 })
   address: string;
 
+  @ApiProperty({description:'備註',example:'到貨前請先電話連絡收件人'})
   @Column({ name: "memo", length: 200 })
   memo: string;
 
+  @ApiProperty({description:'發票統編(發票如需開立統編則填入，無則空白)',example:'23525871'})
   @Column({ name: "vatno", length: 10 })
   vatno: string;
 
+  @ApiProperty({description:'訂單總金額',example:'6980'})
   @Column("decimal", {
     name: "totalamount",
     precision: 8,
@@ -63,6 +72,41 @@ export class Order {
   })
   totalamount: number;
 
+  @ApiProperty({description:'訂單狀態(S01:已付款, S02:已出貨, S03:退貨, S04:已取消)',example:'S01'})
   @Column({ name: "orderstatus", length: 20 })
   orderstatus: string;
+
+  @ApiProperty({
+    description:'訂單明細資料',
+    example:[
+      {
+        flowkey: "1O1110218001",
+        seqNo: "1",
+        productcode: "G021023",
+        batchno: "G021023.SS",
+        productname: "2022好運攏虎你福袋",
+        unit: "袋",
+        qty: "5",
+        price: "1000",
+        amount: "5000",
+      },
+      {
+        flowkey: "1O1110218001",
+        seqNo: "2",
+        productcode: "G021025",
+        batchno: "G021025.SS",
+        productname: "2022 HARRODS年度熊",
+        unit: "隻",
+        qty: "1",
+        price: "1980",
+        amount: "1980",
+      }
+    ]
+  })
+  @OneToMany(
+      () => OrdersDetail, 
+      (ordersDetail) => ordersDetail.master,
+      {cascade: true}   // 關聯details用
+    )
+  details: OrdersDetail[];
 }
--- src/orders/entities/ordersdetail.entity.ts
+++ src/orders/entities/ordersdetail.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
 import { Order } from "./order.entity";
 
@@ -13,21 +14,31 @@ export class OrdersDetail {
   @JoinColumn([{ name: "flowkey", referencedColumnName: "flowkey" }])
   master: Order;
 
+  @ApiProperty({description:'流程代碼',example:'1O1110218001'})
   @Column({ primary: true, name: "flowkey", length: 50 })
   flowkey: string;
 
+  @ApiProperty({description:'明細序號',example:'1'})
   @Column("decimal", { primary: true, name: "seq_no", precision: 8, scale: 0 })
   seqNo: number;
 
+  @ApiProperty({description:'商品料號',example:'G021023'})
   @Column({ name: "productcode", length: 20 })
   productcode: string;
+  
+  @ApiProperty({description:'商品批號',example:'G021023.SS'})
+  @Column({ name: "batchno", length: 20 })
+  batchno: string;
 
+  @ApiProperty({description:'商品名稱',example:'2022好運攏虎你福袋'})
   @Column({ name: "productname", length: 50 })
   productname: string;
 
+  @ApiProperty({description:'單位',example:'袋'})
   @Column({ name: "unit", length: 10 })
   unit: string;
 
+  @ApiProperty({description:'數量',example:'5'})
   @Column("decimal", {
     name: "qty",
     precision: 8,
@@ -36,6 +47,7 @@ export class OrdersDetail {
   })
   qty: number;
 
+  @ApiProperty({description:'單價',example:'1000'})
   @Column("decimal", {
     name: "price",
     precision: 8,
@@ -44,6 +56,7 @@ export class OrdersDetail {
   })
   price: number;
 
+  @ApiProperty({description:'金額',example:'5000'})
   @Column("decimal", {
     name: "amount",
     precision: 8,
